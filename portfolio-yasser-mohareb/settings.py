a04997ba7dffc14f256c31285ed40a2fc23ee1b4
Yasser Mohareb <yakshprod@gmail.com>
2022-06-16 17:37:58 -0400

    server settings

--- portfolio-yasser-mohareb/settings.py
+++ portfolio-yasser-mohareb/settings.py
@@ -141,7 +141,8 @@ STATICFILES_DIRS = (
 # LOCAL SETTINGS
 # STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
