10829cb806e521df20ed38b159e27646273f398a
Yasser Mohareb <yakshprod@gmail.com>
2022-06-17 08:42:04 -0400

    added logging dict in settings.py

--- portfolio-yasser-mohareb/settings.py
+++ portfolio-yasser-mohareb/settings.py
@@ -149,3 +149,19 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
