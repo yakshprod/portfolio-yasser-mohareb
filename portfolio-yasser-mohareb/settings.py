9252a3f6f9aa92da4a6915124f8251b995b280a9
Yasser Mohareb <yakshprod@gmail.com>
2022-06-17 16:52:31 -0400

    removed whitenoise.runserver_nostatic from INSTALLED_APPS, also configured allowed hosts

--- portfolio-yasser-mohareb/settings.py
+++ portfolio-yasser-mohareb/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2ds36+9@#f6+@7tj+uspy_bgvw65h_73e7(#0a7i*$mmb&_r_r
 # Server Settings
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'portfolio-yasser-mohareb.herokuapp.com']
 
 
 # Application definition
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',
+    # 'whitenoise.runserver_nostatic',
     'portfolio',
     'projects',
 ]
@@ -143,7 +143,7 @@ STATICFILES_DIRS = (
 # STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
