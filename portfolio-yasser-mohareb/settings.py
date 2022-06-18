1f7f32e6fc5de446d09c28d1092b0fe2c054951d
Yasser Mohareb <yakshprod@gmail.com>
2022-06-17 21:46:52 -0400

    production settings.py changes

--- portfolio-yasser-mohareb/settings.py
+++ portfolio-yasser-mohareb/settings.py
@@ -144,9 +144,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 
